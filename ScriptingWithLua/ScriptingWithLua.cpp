20d98770d4e24804b504887d821e3534975fc117
ivanzamoraarias <faucar93@gmail.com>
2020-02-15 01:01:09 +0100

    Calling a function from lua to c++

--- ScriptingWithLua/ScriptingWithLua.cpp
+++ ScriptingWithLua/ScriptingWithLua.cpp
@@ -4,15 +4,23 @@
 #include <iostream>
 #include <string>
 extern "C" {
-	#include "Lua/include/lua.h"
-	#include "Lua/include/lauxlib.h"
-	#include "Lua/include/lualib.h"
+#include "Lua/include/lua.h"
+#include "Lua/include/lauxlib.h"
+#include "Lua/include/lualib.h"
 }
 #ifdef _WIN32
 #pragma comment(lib, "Lua/liblua53.a")
 #endif
 
-bool checkLua(lua_State *L, int r) {
+struct Player {
+	std::string role;
+	std::string name;
+	std::string ability;
+	int level;
+
+} player;
+
+bool checkLua(lua_State* L, int r) {
 	if (r == LUA_OK)
 		return true;
 
@@ -24,18 +32,19 @@ bool checkLua(lua_State *L, int r) {
 void finishLua(lua_State* L) {
 	lua_close(L);
 }
+
+int getPlayerObjectFromLua(lua_State*& L, Player& player, bool& retflag);
+
 int main()
 {
-    std::cout << "Lua Scripting !!!! !\n";
+	
 
-	//std::string cmd = "a = 3 + 5 + math.sin(45.0)";
+	std::cout << "Lua Scripting !!!! !\n";
 
 	lua_State* L = luaL_newstate(); // create lua virtual machine
 
 	luaL_openlibs(L);
 
-
-	//int r = luaL_dostring(L, cmd.c_str());
 	int r = luaL_dofile(L, "script.lua");
 
 	if (!checkLua(L, r)) {
@@ -43,24 +52,56 @@ int main()
 		return 0;
 	}
 
-	int sss = lua_getglobal(L, "a");
-	if (lua_isnumber(L, -1)) {
-		float aInCpp = (float)lua_tonumber(L, -1);
-		std::cout << "a in Cplusplus: " << aInCpp << std::endl;
+	lua_getglobal(L, "addSomething");
+	if (lua_isfunction(L, -1)) {
+		lua_pushnumber(L, 3.5f); // a
+		lua_pushnumber(L, 8.5f); // b
+
+		if (checkLua(L, lua_pcall(L, 2, 1, 0))) {
+			float result = (float)lua_tonumber(L, 1);
+			std::cout << "called a function: result=  " << result << std::endl;
+		}
 	}
-	
+
+	/*if (getPlayerObjectFromLua(L, player) == 0)
+		return 0;*/
 
 	finishLua(L);
 	return 0;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+int getPlayerObjectFromLua(lua_State*& L, Player& player)
+{
+	
+	lua_getglobal(L, "player");
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+	if (!lua_istable(L, -1)) {
+		finishLua(L);
+		return 0;
+	}
+
+	lua_pushstring(L, "Name");
+	lua_gettable(L, -2);
+	player.name = lua_tostring(L, -1);
+	lua_pop(L, 1);
+
+	lua_pushstring(L, "Role");
+	lua_gettable(L, -2);
+	player.role = lua_tostring(L, -1);
+	lua_pop(L, 1);
+
+	lua_pushstring(L, "Ability");
+	lua_gettable(L, -2);
+	player.ability = lua_tostring(L, -1);
+	lua_pop(L, 1);
+
+	lua_pushstring(L, "Level");
+	lua_gettable(L, -2);
+	player.level = (int)lua_tonumber(L, -1);
+	lua_pop(L, 1);
+
+
+	std::cout << player.name << " " << player.role << " " << player.ability << " " << player.level << std::endl;
+	
+	return 1;
+}
--- ScriptingWithLua/script.lua
+++ ScriptingWithLua/script.lua
@@ -1,2 +1,25 @@
-a = 3 + 5 + math.sin(45.0)
-a = a + 200
+-- object is a table
+player = {
+	Role = "Ardilla",
+	Name = "Pilla",
+	Ability = "trolldom",
+	Level = 80
+}
+
+players = {}
+player[0] = player
+player[1]= {
+	Role = "Ardilla",
+	Name = "Pilla",
+	Ability = "trolldom",
+	Level = 80}
+
+function addSomething( a, b )
+	print("LUA called with "..a.." , "..b.." final \n")
+	return a+b
+end
+
+function getPlayer(index)
+	return players[index]
+end
+
